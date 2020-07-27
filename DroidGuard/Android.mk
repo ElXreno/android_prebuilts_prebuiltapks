363d7a8be2f83f173cd867e86259b4a4986afd86
ElXreno <elxreno@gmail.com>
2020-07-27 08:21:18 +0300

    DroidGuard: update build
    Built in docker container (circleci/android:api-29).
    Used tag https://github.com/microg/RemoteDroidGuard/releases/tag/v0.1.2

--- DroidGuard/Android.mk
+++ DroidGuard/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := DroidGuard
-LOCAL_SRC_FILES := org.microg.gms.droidguard-25-unsigned.apk
+LOCAL_SRC_FILES := org.microg.gms.droidguard-28-unsigned.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := platform
